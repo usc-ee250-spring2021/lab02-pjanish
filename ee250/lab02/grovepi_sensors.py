31345a324b995cab18eb7feeb0fbda6a27a10ac5
Payton <pjanish@usc.edu>
2021-02-05 21:11:01 +0000

    commit feb 5

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -1,8 +1,11 @@
 """ EE 250L Lab 02: GrovePi Sensors
 
+
 List team members here.
+Payton Janish
 
 Insert Github repository link here.
+https://github.com/usc-ee250-spring2021/lab02-pjanish
 """
 
 """python3 interpreters in Ubuntu (and other linux distros) will look in a 
@@ -23,16 +26,32 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+from grove_rgb_lcd import *
 
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
 if __name__ == '__main__':
-    PORT = 4    # D4
+    USR_PORT = 4    # D4
+    RAS_PORT = 0    # A0
 
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
 
-        print(grovepi.ultrasonicRead(PORT))
+        threshold_dist = grovepi.analogRead(RAS_PORT)
+        object_dist = grovepi.ultrasonicRead(USR_PORT)
+        close = False
+
+        if(object_dist <= threshold_dist):
+             close = True
+        if(close):
+            setRGB(255,0,0)
+            word = str(threshold_dist) + "cm OBJ PRES\n" + str(object_dist) + "cm"
+            setText(word)
+        else:
+            setRGB(0,255,0)
+            new_word = str(threshold_dist) + "cm\n" + str(object_dist) + "cm"
+            setText(new_word)
+
